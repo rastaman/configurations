1618817cfa34e56440d11e22a019d083b26a550b
Dan Mushkevych <mushkevych@gmail.com>
2013-11-27 11:41:46 -0800

    Update hdfs_init.sh

--- CDH4.pseudo-distributed/hdfs_init.sh
+++ CDH4.pseudo-distributed/hdfs_init.sh
@@ -1,28 +1,31 @@
 # system-wide structure
-sudo -u hdfs hadoop fs -mkdir /tmp
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp
+sudo -u hdfs hdfs dfs -mkdir /tmp
+sudo -u hdfs hdfs dfs -chmod -R 1777 /tmp
 
-sudo -u hdfs hadoop fs -mkdir /tmp/hadoop-yarn/staging
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp/hadoop-yarn/staging
+sudo -u hdfs hdfs dfs -mkdir /tmp/hadoop-yarn/staging
+sudo -u hdfs hdfs dfs -chmod -R 1777 /tmp/hadoop-yarn/staging
 
-sudo -u hdfs hadoop fs -mkdir /tmp/hadoop-yarn/staging/history/done_intermediate
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp/hadoop-yarn/staging/history/done_intermediate
+sudo -u hdfs hdfs dfs -mkdir /tmp/hadoop-yarn/staging/history/done_intermediate
+sudo -u hdfs hdfs dfs -chmod -R 1777 /tmp/hadoop-yarn/staging/history/done_intermediate
 
-sudo -u hdfs hadoop fs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
+sudo -u hdfs hdfs dfs -chown -R mapred:mapred /tmp/hadoop-yarn/staging
 
-sudo -u hdfs hadoop fs -mkdir /var/log/hadoop-yarn
-sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn
+sudo -u hdfs hdfs dfs -mkdir /var/log/hadoop-yarn
+sudo -u hdfs hdfs dfs -chown yarn:mapred /var/log/hadoop-yarn
 
 # user-specific structure
-sudo -u hdfs hadoop fs -mkdir /user/bohdan
-sudo -u hdfs hadoop fs -chown bohdan /user/bohdan
+sudo -u hdfs hdfs dfs -mkdir /user/bohdan
+sudo -u hdfs hdfs dfs -chown bohdan /user/bohdan
 
-sudo -u hdfs hadoop fs -mkdir /user/history
-sudo -u hdfs hadoop fs -chmod -R 1777 /user/history
+sudo -u hdfs hdfs dfs -mkdir /user/history
+sudo -u hdfs hdfs dfs -chmod -R 1777 /user/history
 
-sudo -u hdfs hadoop fs -mkdir /user/yarn
-sudo -u hdfs hadoop fs -chown yarn /user/yarn
+sudo -u hdfs hdfs dfs -mkdir /user/yarn
+sudo -u hdfs hdfs dfs -chown yarn /user/yarn
 
 # HBase-specific structure
-sudo -u hdfs hadoop fs -mkdir /hbase
-sudo -u hdfs hadoop fs -chown hbase /hbase
+sudo -u hdfs hdfs dfs -mkdir /hbase
+sudo -u hdfs hdfs dfs -chown hbase /hbase
+
+# print created HDFS filesystem tree structure
+sudo -u hdfs hdfs dfs -ls -R /
