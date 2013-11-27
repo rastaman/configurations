1ca0c9b91fbbf26ce8b57f8ba34c5f5a8c6eee5a
Dan Mushkevych <mushkevych@gmail.com>
2013-11-27 11:37:54 -0800

    Update hdfs_format.sh

--- CDH4.pseudo-distributed/hdfs_format.sh
+++ CDH4.pseudo-distributed/hdfs_format.sh
@@ -1,4 +1,3 @@
-
 # $FS_MOUNT_POINT stands for fully qualified Linux filesystem path 
 # to the mount point where Hadoop folders and structures reside.
 # For instance: /media/_media_1TB
@@ -11,4 +10,8 @@ sudo chown -R hdfs:hadoop $FS_MOUNT_POINT/hadoop_tmp
 sudo sudo mkdir --mode=777 $FS_MOUNT_POINT/zookeeper
 sudo chown -R zookeeper:zookeeper $FS_MOUNT_POINT/zookeeper
 
+# initializes Zookeeper
+sudo service zookeeper-server init
+
+# formats HDFS namenode
 sudo -u hdfs hdfs namenode -format
